41256e2a9a37b45c528d37f86b191e1c6e698dfb
allys <allysyaqottrunnada24@gmail.com>
2023-07-30 10:52:22 +0700

    Add files via upload

---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="carousel">
+    <transition-group class="slide"> </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductDisplay",
+  data() {
+    return {
+      items: [],
+    };
+  },
+};
+</script>
